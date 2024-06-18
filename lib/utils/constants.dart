5742b75c4e70bd8bad635fc317523daf4509d8d7
Arun <arun07aj@gmail.com>
2024-06-18 20:13:55 +0530

    added constants file

---
+++ lib/utils/constants.dart
@@ -0,0 +1,18 @@
+const List<String> daysOfWeek = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+
+const int minHour = 0;
+const int maxHour = 23;
+
+const int minMinute = 0;
+const int maxMinute = 59;
+
+const int minSec = 0;
+const int maxSec = 59;
